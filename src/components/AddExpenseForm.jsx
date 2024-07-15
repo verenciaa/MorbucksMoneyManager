ba4423f43ddece3b51771524b4027c1e08add1ff
Irfan Saf <irfansaf7@gmail.com>
2024-07-15 22:47:33 +0700

    feat: ocr scan receipt by use jakartaresearch modules

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -1,123 +1,161 @@
-// react imports
-import { useEffect, useRef } from "react"
-
-// rrd imports
-import { useFetcher } from "react-router-dom"
-
-// library imports
-import { PlusCircleIcon } from "@heroicons/react/24/solid"
+import { useEffect, useRef, useState } from "react";
+import { useFetcher } from "react-router-dom";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
+import Modal from "react-modal";
+import { ocrPredict } from "../helpers.js";
 
 const AddExpenseForm = ({ budgets }) => {
-  const fetcher = useFetcher()
-  const isSubmitting = fetcher.state === "submitting";
+    const fetcher = useFetcher();
+    const isSubmitting = fetcher.state === "submitting";
 
-  const formRef = useRef()
-  const focusRef = useRef()
+    const formRef = useRef();
+    const focusRef = useRef();
 
-  useEffect(() => {
-    if (!isSubmitting) {
-      // clear form
-      formRef.current.reset()
-      // reset focus
-      focusRef.current.focus()
-    }
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [isScanning, setIsScanning] = useState(false);
 
-  }, [isSubmitting])
+    useEffect(() => {
+        if (!isSubmitting) {
+            formRef.current.reset();
+            focusRef.current.focus();
+        }
+    }, [isSubmitting]);
 
-  return (
-    <div className="form-wrapper">
-      <h2 className="h3">Add New{" "}<span className="accent">
-        {budgets.length === 1 && `${budgets.map((budg) => budg.name)}`}
-      </span>{" "}
-        Expense
-      </h2>
-      <fetcher.Form
-        method="post"
-        className="grid-sm"
-        ref={formRef}
-      >
-        <div className="expense-inputs">
-          <div className="grid-xs">
-            <label htmlFor="newExpense">Expense Name</label>
-            <input
-              type="text"
-              name="newExpense"
-              id="newExpense"
-              placeholder="e.g., Strawberries, Tomatoes, etc."
-              ref={focusRef}
-              required
-            />
-          </div>
-          <div className="grid-xs">
-            <label htmlFor="newExpenseAmount">Amount</label>
-            <input
-              type="number"
-              step="0.01"
-              inputMode="decimal"
-              name="newExpenseAmount"
-              id="newExpenseAmount"
-              placeholder="e.g., 50.000"
-              required
-            />
-          </div>
-        </div>
-        <div className="grid-xs" hidden={budgets.length === 1}>
-          <label htmlFor="newExpenseBudget">Budget Category</label>
-          <select name="newExpenseBudget" id="newExpenseBudget" required>
-            {
-              budgets
-                .sort((a, b) => a.createdAt - b.createdAt)
-                .map((budget) => {
-                  return (
-                    <option key={budget.id} value={budget.id}>
-                      {budget.name}
-                    </option>
-                  )
-                })
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    const handleScanReceipt = async () => {
+        if (!selectedFile) return;
+
+        setIsScanning(true);
+        const reader = new FileReader();
+
+        reader.onloadend = async () => {
+            const imageBase64 = reader.result.split(",")[1];
+            try {
+                const response = await ocrPredict(imageBase64);
+                console.log("OCR Response:", response);
+
+                // Function to search for total or subtotal amount
+                const findAmount = (lines) => {
+                    const totalRegex = /(TOTAL|SUBTOTAL)\s*:?\s*([\d,]+)/i; // Regex to match "TOTAL 14,000" or "SUBTOTAL: 14,000" (with optional colon)
+                    for (let line of lines) {
+                        const match = totalRegex.exec(line);
+                        if (match) {
+                            return parseFloat(match[2].replace(",", ""));
+                        }
+                    }
+                    return null;
+                };
+
+                // Find total or subtotal amount from OCR response
+                const amount = findAmount(response);
+
+                if (amount) {
+                    formRef.current.newExpenseAmount.value = amount;
+                } else {
+                    console.log("No total or subtotal found in OCR response.");
+                    // Handle case where no total or subtotal is found
+                }
+            } catch (error) {
+                console.error("Error scanning receipt:", error);
+            } finally {
+                setIsScanning(false);
+                closeModal();
             }
-          </select>
+        };
+
+        reader.readAsDataURL(selectedFile);
+    };
+
+
+
+    return (
+        <div className="form-wrapper">
+            <h2 className="h3">Add New{" "}
+                <span className="accent">
+                    {budgets.length === 1 && `${budgets.map((budg) => budg.name)}`}
+                </span>{" "}
+                Expense
+            </h2>
+            <fetcher.Form method="post" className="grid-sm" ref={formRef}>
+                <div className="expense-inputs">
+                    <div className="grid-xs">
+                        <label htmlFor="newExpense">Expense Name</label>
+                        <input
+                            type="text"
+                            name="newExpense"
+                            id="newExpense"
+                            placeholder="e.g., Strawberries, Tomatoes, etc."
+                            ref={focusRef}
+                            required
+                        />
+                    </div>
+                    <div className="grid-xs">
+                        <label htmlFor="newExpenseAmount">Amount</label>
+                        <input
+                            type="number"
+                            step="0.01"
+                            inputMode="decimal"
+                            name="newExpenseAmount"
+                            id="newExpenseAmount"
+                            placeholder="e.g., 50.000"
+                            required
+                        />
+                    </div>
+                </div>
+                <div className="grid-xs" hidden={budgets.length === 1}>
+                    <label htmlFor="newExpenseBudget">Budget Category</label>
+                    <select name="newExpenseBudget" id="newExpenseBudget" required>
+                        {budgets
+                            .sort((a, b) => a.createdAt - b.createdAt)
+                            .map((budget) => (
+                                <option key={budget.id} value={budget.id}>
+                                    {budget.name}
+                                </option>
+                            ))}
+                    </select>
+                </div>
+                <input type="hidden" name="_action" value="createExpense" />
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <button type="submit" className="btn btn--dark" disabled={isSubmitting}>
+                        {isSubmitting ? <span>Submitting…</span> : (
+                            <>
+                                <span>Add Expense</span>
+                                <PlusCircleIcon width={20} />
+                            </>
+                        )}
+                    </button>
+                    <button type="button" className="btn btn--dark" onClick={openModal} disabled={isScanning}>
+                        {isScanning ? <span>Scanning…</span> : (
+                            <>
+                                <span>Scan your receipt</span>
+                                <PlusCircleIcon width={20} />
+                            </>
+                        )}
+                    </button>
+                </div>
+            </fetcher.Form>
+            <Modal
+                isOpen={isModalOpen}
+                onRequestClose={closeModal}
+                contentLabel="Scan Receipt Modal"
+                ariaHideApp={false}
+            >
+                <h2>Upload Receipt</h2>
+                <input type="file" accept="image/*" onChange={handleFileChange} />
+                <button onClick={handleScanReceipt} disabled={isScanning || !selectedFile}>
+                    {isScanning ? "Scanning..." : "Scan"}
+                </button>
+                <button onClick={closeModal}>Close</button>
+            </Modal>
         </div>
-        <input type="hidden" name="_action" value="createExpense" />
-        {/* <button type="submit" className="btn btn--dark" disabled={isSubmitting}>
-          {
-            isSubmitting ? <span>Submitting…</span> : (
-              <>
-                <span>Add Expense</span>
-                <PlusCircleIcon width={20} />
-              </>
-            )
-          }
-        </button>
-        <button type="submit" className="btn btn--dark" disabled={isSubmitting}>
-          {
-            isSubmitting ? <span>Submitting…</span> : (
-              <>
-                <span>Scan your receipt</span>
-                <PlusCircleIcon width={20} />
-              </>
-            )
-          }
-        </button> */}
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <button type="submit" className="btn btn--dark" disabled={isSubmitting}>
-            {isSubmitting ? <span>Submitting…</span> : (
-              <>
-                <span>Add Expense</span>
-                <PlusCircleIcon width={20} />
-              </>
-            )}
-          </button>
-          <button type="submit" className="btn btn--dark" disabled={isSubmitting}>
-            {isSubmitting ? <span>Submitting…</span> : (
-              <>
-                <span>Scan your receipt</span>
-                <PlusCircleIcon width={20} />
-              </>
-            )}
-          </button>
-        </div>
-      </fetcher.Form>
-    </div>
-  )
-}
-export default AddExpenseForm
+    );
+};
+
+export default AddExpenseForm;
--- src/helpers.js
+++ src/helpers.js
@@ -1,109 +1,124 @@
+import axios from "axios";
+
 export const waait = () =>
-  new Promise((res) => setTimeout(res, Math.random() * 800));
+    new Promise((res) => setTimeout(res, Math.random() * 800));
 
 // colors
 const generateRandomColor = () => {
-  const existingBudgetLength = fetchData("budgets")?.length ?? 0;
-  return `${existingBudgetLength * 34} 65% 50%`;
+    const existingBudgetLength = fetchData("budgets")?.length ?? 0;
+    return `${existingBudgetLength * 34} 65% 50%`;
 };
 
 // Local storage
 export const fetchData = (key) => {
-  return JSON.parse(localStorage.getItem(key));
+    return JSON.parse(localStorage.getItem(key));
 };
 
 // Get all items from local storage
-export const getAllMatchingItems = ({ category, key, value }) => {
-  const data = fetchData(category) ?? [];
-  return data.filter((item) => item[key] === value);
+export const getAllMatchingItems = ({category, key, value}) => {
+    const data = fetchData(category) ?? [];
+    return data.filter((item) => item[key] === value);
 };
 
 // delete item from local storage
-export const deleteItem = ({ key, id }) => {
-  const existingData = fetchData(key);
-  if (id) {
-    const newData = existingData.filter((item) => item.id !== id);
-    return localStorage.setItem(key, JSON.stringify(newData));
-  }
-  return localStorage.removeItem(key);
+export const deleteItem = ({key, id}) => {
+    const existingData = fetchData(key);
+    if (id) {
+        const newData = existingData.filter((item) => item.id !== id);
+        return localStorage.setItem(key, JSON.stringify(newData));
+    }
+    return localStorage.removeItem(key);
 };
 
 // create budget
-export const createBudget = ({ name, amount }) => {
-  const newItem = {
-    id: crypto.randomUUID(),
-    name: name,
-    createdAt: Date.now(),
-    amount: +amount,
-    color: generateRandomColor(),
-  };
-  const existingBudgets = fetchData("budgets") ?? [];
-  return localStorage.setItem(
-    "budgets",
-    JSON.stringify([...existingBudgets, newItem])
-  );
+export const createBudget = ({name, amount}) => {
+    const newItem = {
+        id: crypto.randomUUID(),
+        name: name,
+        createdAt: Date.now(),
+        amount: +amount,
+        color: generateRandomColor(),
+    };
+    const existingBudgets = fetchData("budgets") ?? [];
+    return localStorage.setItem(
+        "budgets",
+        JSON.stringify([...existingBudgets, newItem])
+    );
 };
 
 // create expense
-export const createExpense = ({ name, amount, budgetId }) => {
-  const newItem = {
-    id: crypto.randomUUID(),
-    name: name,
-    createdAt: Date.now(),
-    amount: +amount,
-    budgetId: budgetId,
-  };
-  const existingExpenses = fetchData("expenses") ?? [];
-  return localStorage.setItem(
-    "expenses",
-    JSON.stringify([...existingExpenses, newItem])
-  );
+export const createExpense = ({name, amount, budgetId}) => {
+    const newItem = {
+        id: crypto.randomUUID(),
+        name: name,
+        createdAt: Date.now(),
+        amount: +amount,
+        budgetId: budgetId,
+    };
+    const existingExpenses = fetchData("expenses") ?? [];
+    return localStorage.setItem(
+        "expenses",
+        JSON.stringify([...existingExpenses, newItem])
+    );
 };
 
 // total spent by budget
 export const calculateSpentByBudget = (budgetId) => {
-  const expenses = fetchData("expenses") ?? [];
-  const budgetSpent = expenses.reduce((acc, expense) => {
-    // check if expense.id === budgetId I passed in
-    if (expense.budgetId !== budgetId) return acc;
+    const expenses = fetchData("expenses") ?? [];
+    const budgetSpent = expenses.reduce((acc, expense) => {
+        // check if expense.id === budgetId I passed in
+        if (expense.budgetId !== budgetId) return acc;
 
-    // add the current amount to my total
-    return (acc += expense.amount);
-  }, 0);
-  return budgetSpent;
+        // add the current amount to my total
+        return (acc += expense.amount);
+    }, 0);
+    return budgetSpent;
 };
 
 // FORMATTING
 export const formatDateToLocaleString = (epoch) =>
-  new Date(epoch).toLocaleDateString();
+    new Date(epoch).toLocaleDateString();
 
 // Formating percentages
 export const formatPercentage = (amt) => {
-  return amt.toLocaleString(undefined, {
-    style: "percent",
-    minimumFractionDigits: 0,
-  });
+    return amt.toLocaleString(undefined, {
+        style: "percent",
+        minimumFractionDigits: 0,
+    });
 };
 
 // Format currency
 export const formatCurrency = (amt) => {
-  return amt.toLocaleString(undefined, {
-    style: "currency",
-    currency: "IDR",
-  });
+    return amt.toLocaleString(undefined, {
+        style: "currency",
+        currency: "IDR",
+    });
 };
 
-import Tesseract from 'tesseract.js';
+// ocrPredict fetch from http://localhost:5000/ocr-predict
+export const ocrPredict = async (imageBase64) => {
 
-export const processReceipt = (imageFile) => {
-  return Tesseract.recognize(
-    imageFile,
-    'eng',
-    {
-      logger: (m) => console.log(m),
-    }
-  ).then(({ data: { text } }) => {
-    console.log(text);
-    return text;
-  });
-};
+    const response = await axios.post("http://127.0.0.1:8000/ocr/predict", {
+        image: imageBase64,
+    });
+
+    console.log(response)
+
+    return response.data;
+}
+
+//
+// import Tesseract from 'tesseract.js';
+//
+// export const processReceipt = (imageFile) => {
+//   return Tesseract.recognize(
+//     imageFile,
+//     'eng',
+//     {
+//       logger: (m) => console.log(m),
+//     }
+//   ).then(({ data: { text } }) => {
+//     console.log(text);
+//     return text;
+//   });
+// };
